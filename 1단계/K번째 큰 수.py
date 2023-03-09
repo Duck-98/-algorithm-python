65170ceccd469eb45f1fec89cf46fdc17017685a
Duck-98 <72850354+Duck-98@users.noreply.github.com>
2023-03-10 01:37:49 +0900

    Lv1: K번째 큰 수

---
+++ 1단계/K번째 큰 수.py
@@ -0,0 +1,35 @@
+'''
+현수는 1부터 100사이의 자연수가 적힌 N장의 카드를 가지고 있습니다. 같은 숫자의 카드가 
+여러장 있을 수 있습니다. 현수는 이 중 3장을 뽑아 각 카드에 적힌 수를 합한 값을 기록하려
+고 합니다. 3장을 뽑을 수 있는 모든 경우를 기록합니다. 기록한 값 중 K번째로 큰 수를 출력
+하는 프로그램을 작성하세요.
+만약 큰 수부터 만들어진 수가 25 25 23 23 22 20 19......이고 K값이 3이라면 K번째 큰 값
+은 22입니다.
+
+'''
+
+import sys
+sys.stdin=open("in1.txt", "rt")
+
+n, k = map(int,input().split())
+print(n,k)
+a = list(map(int,input().split()))
+print(a)
+res=set() 
+# set()=> 같은 값을 여러번 넣어도 한번만 들어감.
+for i in range(n):
+    for j in range(i+1,n):
+        for m in range(j+1, n):
+            res.add(a[i]+a[j]+a[m])
+
+res=list(res)
+# sort함수를 쓰기 위해서 튜플구조 -> 리스트구조
+res.sort(reverse=True) # 내림차순
+print(res)
+print(res[k-1])
+
+'''
+1.input값을 list화하거나 int로 불러옴.
+2.문제에 맞게 풀어줌
+'''
+
