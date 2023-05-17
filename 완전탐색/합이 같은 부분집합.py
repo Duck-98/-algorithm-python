7615e72c0fa042f81c0d57ff58e5e1bbab254696
Duck-98 <72850354+Duck-98@users.noreply.github.com>
2023-05-18 01:55:46 +0900

    DFS: 합이 같은 부분 집합

--- 완전탐색/합이 같은 부분집합.py
+++ 완전탐색/합이 같은 부분집합.py
@@ -1,19 +1,30 @@
-import sys
+import sys 
 sys.stdin=open("input.txt", "r")
-def DFS(L, sum):
-    if sum>total//2:a
-        return
-    if L==n:
-        if sum==(total-sum):
-            print("YES")
-            sys.exit(0)
-    else:
-        DFS(L+1, sum+a[L])
-        DFS(L+1, sum)
+
+def DFS(L, res):
+   if res>total//2:
+       return
+   
+   if L==n :
+       if res==(total-res):
+           print("YES")
+           sys.exit(0)
+   else:
+       DFS(L+1, res+a[L])
+       DFS(L+1, res)
+       
+    
+       
+
+         
+
 
 if __name__=="__main__":
     n=int(input())
-    a=list(map(int, input().split()))
+    a = list(map(int, input().split()))
     total=sum(a)
     DFS(0, 0)
     print("NO")
+    
+       
+   
